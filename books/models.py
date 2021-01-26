38aebc5e3540778667ed29aa298434c5f3439d93
KariukiAbel <abelmuriithi@gmail.com>
2021-01-26 16:33:30 +0300

    Learned how to create tables using the models.py file

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#creates a table in db.sqlite
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    
+    
+#syntax for creating a table 
+# class table_name(models.Model):
+#     attributes come here
