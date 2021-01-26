ccb41a252ffcd2ddecf5d80291a55f63bbdcb683
KariukiAbel <abelmuriithi@gmail.com>
2021-01-26 16:20:17 +0300

    The currate is nolonger required in the new versions of django

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^books/', include('books.urls')),
+    path(r'books/', include('books.urls')),
 ]
