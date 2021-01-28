365eb55b75b923d2dd3d7f138c13b07578230b76
KariukiAbel <abelmuriithi@gmail.com>
2021-01-28 23:13:01 +0300

    Learned about templates and how to render them on using python and django

--- books/views.py
+++ books/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     #To get all books in the db
     all_books = Book.objects.all()
-    html = ''
-    for book in all_books:
-        url = '/books/' + str(book.id) + '/'
-        html+= '<a href="'+ url +'">' + str(book.name) + '</a><br>'
-    # return HttpResponse("<h>This is the books homepage</h>")
-    return HttpResponse(html)
+    # html = ''
+    # for book in all_books:
+    #     url = '/books/' + str(book.id) + '/'
+    #     html+= '<a href="'+ url +'">' + str(book.name) + '</a><br>'
+    # # return HttpResponse("<h>This is the books homepage</h>")
+    # return HttpResponse(html)
+    template = loader.get_template('books/index.html')
+    context={
+        'all_books':all_books
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, book_id):
     return HttpResponse("<h2>Details for Book ID: " + str(book_id) + "</h2>")
