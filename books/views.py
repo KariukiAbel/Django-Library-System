d3330aa4049189ed874147e959d014c188a27c5c
KariukiAbel <abelmuriithi@gmail.com>
2021-01-28 23:21:23 +0300

    Understood how to use the render function from django.shortcuts to render templates on the browser

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -14,11 +15,13 @@ def index(request):
     #     html+= '<a href="'+ url +'">' + str(book.name) + '</a><br>'
     # # return HttpResponse("<h>This is the books homepage</h>")
     # return HttpResponse(html)
-    template = loader.get_template('books/index.html')
+    # template = loader.get_template('books/index.html')
     context={
         'all_books':all_books
     }
-    return HttpResponse(template.render(context, request))
-
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'books/index.html', context)
+    
+    
 def detail(request, book_id):
     return HttpResponse("<h2>Details for Book ID: " + str(book_id) + "</h2>")
