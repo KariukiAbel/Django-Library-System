9e1ba259427841ce644fd941fa7571ff7113cc5a
KariukiAbel <abelmuriithi@gmail.com>
2021-01-29 00:24:14 +0300

    Just used namespaces to differenciate different files and resources

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'<book_id>/', views.detail, name='detail')
